cf9b4b415c8f95fdbd228682286434a93711c28e
Katapon <q.katapol@gmail.com>
2019-03-21 01:23:15 +0700

    character can move.

--- field.py
+++ field.py
@@ -1,15 +1,43 @@
 import arcade
+from models import Character, World
 
 SCREEN_WIDTH = 600
 SCREEN_HEIGHT = 600
+
+
+class ModelSprite(arcade.Sprite):
+    def __init__(self, *args, **kwargs):
+        self.model = kwargs.pop('model', None)
+
+        super().__init__(*args, **kwargs)
+
+    def sync_with_model(self):
+        if self.model:
+            self.set_position(self.model.x, self.model.y)
+
+    def draw(self):
+        self.sync_with_model()
+        super().draw()
+
 
 class FieldWindow(arcade.Window):
     def __init__(self, width, height):
         super().__init__(width, height)
 
-        arcade.set_background_color(arcade.color.GRAY)
+        arcade.set_background_color(arcade.color.BLACK_OLIVE)
+
+        self.world = World(SCREEN_WIDTH, SCREEN_HEIGHT)
+        self.character_sprite = ModelSprite('images/character.png', model=self.world.character)
+
+    def on_key_press(self, key, key_modifiers):
+        self.world.on_key_press(key, key_modifiers)
+
+    def update(self, delta):
+        self.world.update(delta)
+
     def on_draw(self):
         arcade.start_render()
+        self.character_sprite.draw()
 
 def main():
     window = FieldWindow(SCREEN_WIDTH, SCREEN_HEIGHT)
--- models.py
+++ models.py
@@ -1,19 +1,47 @@
+import arcade.key
 
-class Field:
+DIR_STILL = 0
+DIR_UP = 1
+DIR_RIGHT = 2
+DIR_DOWN = 3
+DIR_LEFT = 4
+
+DIR_OFFSETS = {DIR_STILL: (0, 0),
+               DIR_UP: (0, 1),
+               DIR_RIGHT: (1, 0),
+               DIR_DOWN: (0, -1),
+               DIR_LEFT: (-1, 0)}
+
+class Character:
     def __init__(self, world, x, y):
         self.world = world
         self.x = x
         self.y = y
+        self.direction = DIR_STILL
+
+    def move(self, direction):
+        self.x += DIR_OFFSETS[direction][0] * 10
+        self.y += DIR_OFFSETS[direction][1] * 10
 
     def update(self, delta):
-        pass
+        self.move(self.direction)
 
 class World:
     def __init__(self, width, height):
         self.width = width
         self.height = height
 
-        self.field = Field(width // 2, height // 2)
+        self.character = Character(self, width // 2, height // 2)
+
+    def on_key_press(self, key, key_modifiers):
+        if key == arcade.key.UP:
+            self.character.direction = DIR_UP
+        if key == arcade.key.DOWN:
+            self.character.direction = DIR_DOWN
+        if key == arcade.key.LEFT:
+            self.character.direction = DIR_LEFT
+        if key == arcade.key.RIGHT:
+            self.character.direction = DIR_RIGHT
 
     def update(self, delta):
-        self.field.update(delta)
+        self.character.update(delta)
